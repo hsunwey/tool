12bf66e3a2fbaf4d467b5b4ea3102a10da525587
Xun Wei <hectoryeeos@gmail.com>
2020-12-19 12:48:55 +0800

    Add one line command

--- resize-video.sh
+++ resize-video.sh
@@ -1,6 +1,9 @@
-videos=$(find -name "*.mp4" -size +700M)
+videos=$(find -name "*.mp4" -size +700M); for i in $videos;  do  echo $i; mv $i $i.mp4; ffmpeg -i $i.mp4 -s 850x480 -c:a copy $i -hide_banner -loglevel panic; done
 
+videos=$(find -name "*.mp4" -size +700M)
 for i in $videos;
  do
  ffmpeg -i $i -s 850x480 -c:a copy $i.mp4;
 done
+
+# find -size +700M
