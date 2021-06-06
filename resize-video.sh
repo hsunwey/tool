cc55abe7026e0061a08086a8c43031f1efe0bf94
Xun Wei <hectoryeeos@gmail.com>
2021-06-06 20:00:53 +0800

    Revert to folder tead of list

--- resize-video.sh
+++ resize-video.sh
@@ -1,15 +1,16 @@
-videos=($(find -name "*.mp4" -size +700M)); for index in ${!videos[@]}; do echo $((index+1))/${#videos[@]} = "${videos[index]}"; mv $i $i.mp4; ffmpeg -i $i -s 850x480 -c:a copy $i -hide_banner -loglevel panic; done
+videos=$(find -name "*.mp4" -size +700M); for i in $videos;  do  echo $i; mv $i $i.mp4; ffmpeg -i $i.mp4 -s 850x480 -c:a copy $i -hide_banner -loglevel panic; done
 
-videos=($(find -name "*.mp4" -size +700M));
-for index in ${!videos[@]}; do
-    echo $((index+1))/${#videos[@]} = "${videos[index]}";
-    mv $i $i.mp4;
-    ffmpeg -i $i -s 850x480 -c:a copy $i -hide_banner -loglevel panic;
+videos=$(find -name "*.mp4" -size +700M)
+for i in $videos;
+ do
+ ffmpeg -i $i -s 850x480 -c:a copy $i.mp4;
 done
 
-# videos=$(find -name "*.mp4" -size +700M)
-# for i in $videos;
-#  do
-#  ffmpeg -i $i -s 850x480 -c:a copy $i.mp4;
-# done
 # find -size +700M
+
+# videos=($(find -name "*.mp4" -size +700M));
+# for index in ${!videos[@]}; do
+#     echo $((index+1))/${#videos[@]} = "${videos[index]}";
+#     mv $i $i.mp4;
+#     ffmpeg -i $i -s 850x480 -c:a copy $i -hide_banner -loglevel panic;
+# done
