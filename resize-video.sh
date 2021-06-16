6c4546cb1192dc53d94445daa8cb727a7eff0538
Xun Wei <hectoryeeos@gmail.com>
2021-06-16 16:00:19 +0800

    Handle space in filenames

--- resize-video.sh
+++ resize-video.sh
@@ -1,10 +1,15 @@
 videos=$(find -name "*.mp4" -size +700M); for i in $videos;  do  echo $i; mv $i $i.mp4; ffmpeg -i $i.mp4 -s 850x480 -c:a copy $i -hide_banner -loglevel panic; done
 
-videos=$(find -name "*.mp4" -size +700M)
-for i in $videos;
- do
- ffmpeg -i $i -s 850x480 -c:a copy $i.mp4;
+OIFS="$fileFS"
+IFS=$'\n'
+for file in `find . -type f -name "*.mp4" -size +800M`  
+do
+     echo "$file";
+     mv "$file" "$file.mp4";
+     ffmpeg -i "$file.mp4" -s 850x480 -c:a copy "$file" -hide_banner -loglevel panic;
 done
+IFS="$OIFS"
+
 
 # find -size +700M
 
